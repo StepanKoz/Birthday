9e930fba70863ef13e9dc81814dc2ec5df969d63
Stepan <121403794+StepanKoz@users.noreply.github.com>
2024-06-28 23:24:40 +0400

    commit

--- contract.js
+++ contract.js
@@ -1,26 +1,36 @@
 const { TonClient, Address, beginCell, TupleItemSlice } = require("ton");
 
-async function getMessage(contractAddress) {
+async function getPart(contractAddress, methodName) {
     const client = new TonClient({
         endpoint: "https://testnet.toncenter.com/api/v2/jsonRPC",
     });
 
-    
-    await new Promise(resolve => setTimeout(resolve, 1500));
-
     try {
-        const response = await client.runMethod(Address.parse(contractAddress), "get_message", []);
+        const response = await client.runMethod(Address.parse(contractAddress), methodName, []);
         
-
+       
         const message = response.stack.readString();
         
-        console.log("Message from contract:", message);
+        console.log(`Message from ${methodName}:`, message);
     } catch (error) {
-        console.error('Error:', error);
+        console.error(`Error in ${methodName}:`, error);
     }
 }
 
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-const contractAddress = 'EQB2EUP5h8ECDd9lso82x-Vx-UdeNFIlTpuQWViGqf36tuqw';
-getMessage(contractAddress);
 
+const contractAddress = 'EQDdabkeai_cVLez6CD-10NfnvGyuvqMzKOCp-cEFhCeqQ0n';
+
+async function callMethodsWithDelay() {
+    const methods = ['get_part1', 'get_part2', 'get_part3', 'get_part4', 'get_part5', 'get_part6', 'get_part7'];
+
+    for (const method of methods) {
+        await getPart(contractAddress, method);
+        await delay(1500); 
+    }
+}
+
+callMethodsWithDelay();
